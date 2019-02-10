e49666e0ebd27fdf99aadfaaf164378288de701e
Artem Meleshko <>
2019-02-10 12:17:18 +0200

    added framework

---
+++ SMB4iOSFramework/SMB4iOSFramework.h
@@ -0,0 +1,19 @@
+//
+//  SMB4iOSFramework.h
+//  SMB4iOSFramework
+//
+//  Created by Artem on 2/10/19.
+//  Copyright © 2019 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SMB4iOSFramework.
+FOUNDATION_EXPORT double SMB4iOSFrameworkVersionNumber;
+
+//! Project version string for SMB4iOSFramework.
+FOUNDATION_EXPORT const unsigned char SMB4iOSFrameworkVersionString[];
+
+#import <SMB4iOSFramework/NetBios.h>
+#import <SMB4iOSFramework/SmbConnection.h>
+
