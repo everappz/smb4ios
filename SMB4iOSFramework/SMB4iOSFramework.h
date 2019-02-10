07b2ce355ce025b7483cdf5a65d2322de4743571
Artem Meleshko <>
2019-02-10 14:15:11 +0200

    minor

--- SMB4iOSFramework/SMB4iOSFramework.h
+++ SMB4iOSFramework/SMB4iOSFramework.h
@@ -15,5 +15,39 @@ FOUNDATION_EXPORT double SMB4iOSFrameworkVersionNumber;
 FOUNDATION_EXPORT const unsigned char SMB4iOSFrameworkVersionString[];
 
 #import <SMB4iOSFramework/NetBios.h>
+#import <SMB4iOSFramework/NetBiosQuery.h>
+#import <SMB4iOSFramework/RapMessage.h>
+#import <SMB4iOSFramework/RapNetServerEnum2.h>
+#import <SMB4iOSFramework/RapNetServerInfo1.h>
 #import <SMB4iOSFramework/SmbConnection.h>
+#import <SMB4iOSFramework/DceRpc.h>
+#import <SMB4iOSFramework/DceRpcBind.h>
+#import <SMB4iOSFramework/DceRpcClosePrinter.h>
+#import <SMB4iOSFramework/DceRpcData.h>
+#import <SMB4iOSFramework/DceRpcDefines.h>
+#import <SMB4iOSFramework/DceRpcEndDocPrinter.h>
+#import <SMB4iOSFramework/DceRpcEnumAll.h>
+#import <SMB4iOSFramework/DceRpcGetPrinter.h>
+#import <SMB4iOSFramework/DceRpcMessage.h>
+#import <SMB4iOSFramework/DceRpcOpenPrinterEx.h>
+#import <SMB4iOSFramework/DceRpcPrinterInfo2.h>
+#import <SMB4iOSFramework/DceRpcStartDocPrinter.h>
+#import <SMB4iOSFramework/DceRpcWritePrinter.h>
+#import <SMB4iOSFramework/SmbDefines.h>
+#import <SMB4iOSFramework/SmbLogoffMessage.h>
+#import <SMB4iOSFramework/SmbMessage.h>
+#import <SMB4iOSFramework/SmbNegotiateProtocolMessage.h>
+#import <SMB4iOSFramework/SmbNtCreateMessage.h>
+#import <SMB4iOSFramework/SmbReadMessage.h>
+#import <SMB4iOSFramework/SmbSessionSetupMessage.h>
+#import <SMB4iOSFramework/SmbTransactionMessage.h>
+#import <SMB4iOSFramework/SmbTreeConnectMessage.h>
+#import <SMB4iOSFramework/SmbTreeDisconnectMessage.h>
+#import <SMB4iOSFramework/SmbWriteMessage.h>
+#import <SMB4iOSFramework/SmbCloseMessage.h>
+#import <SMB4iOSFramework/NbtConnection.h>
+#import <SMB4iOSFramework/NSMutableData+SMB.h>
+#import <SMB4iOSFramework/SMB4iOSAsyncUdpSocket.h>
+#import <SMB4iOSFramework/SocketConnection.h>
+#import <SMB4iOSFramework/Utils.h>
 
