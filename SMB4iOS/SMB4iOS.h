da2871267b9e78aef635e52c7412f2011ab9c9c2
Artem Meleshko <>
2019-02-10 11:59:18 +0200

    added new target

---
+++ SMB4iOS/SMB4iOS.h
@@ -0,0 +1,13 @@
+//
+//  SMB4iOS.h
+//  SMB4iOS
+//
+//  Created by Artem on 2/10/19.
+//  Copyright © 2019 none. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMB4iOS : NSObject
+
+@end
